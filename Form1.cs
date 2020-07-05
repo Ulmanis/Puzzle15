e88a9756447e112a6415d72c5054bb4bf379e436
Ulmanis <linards.kupcis@gmail.com>
2020-07-05 17:02:43 +0300

    ver 05 07

--- Form1.cs
+++ Form1.cs
@@ -34,12 +34,20 @@ namespace Puzzle15
             {
                 for (int col = 1; col < 5; col++)
                 {
-                    block = new PuzzleBlock();
-                    block.Top = row * 84;
-                    block.Left = col * 84;
-                    block.Text = blockCount.ToString();
+                    block = new PuzzleBlock()
+                    {
+                        Top = row * 84,
+                        Left = col * 84,
+                        Text = blockCount.ToString(),
+                        Name = "Block" + blockCount.ToString()
+                    };
+
+                    //block.Click += new EventHandler(Block_Click);
+                    block.Click += Block_Click;
+
                     if(blockCount == 16)
                     {
+                        block.Name = "EmptyBlock";
                         block.Text = string.Empty;
                         block.BackColor = Color.DarkSlateBlue;
                         block.FlatStyle = FlatStyle.Flat;
@@ -49,6 +57,40 @@ namespace Puzzle15
                     this.Controls.Add(block);
                 }
                 
+            }
+        }
+        private void Block_Click(object sender, EventArgs e)
+        {
+            Button block = (Button)sender;
+            if (IsAdjacent(block))
+            {
+                SwapBlocks(block);
+            }
+        }
+        private void SwapBlocks(Button block)
+        {
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+            Point oldLocation = block.Location;
+            block.Location = emptyBlock.Location;
+            emptyBlock.Location = oldLocation;
+        }
+        private bool IsAdjacent(Button block)
+        {
+            double a;
+            double b;
+            double c;
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+
+            a = Math.Abs(emptyBlock.Top - block.Top);
+            b = Math.Abs(emptyBlock.Left - block.Left);
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            if(c < 85)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
